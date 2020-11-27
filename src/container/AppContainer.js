ff9ab1d6c49487b7cfebb077b19c7f77b1cf485e
Japher Su <japhersu@Japhers-MacBook-Pro.local>
2020-11-27 17:49:21 -0500

    Added property list page; Fixing styling issues

--- src/container/AppContainer.js
+++ src/container/AppContainer.js
@@ -9,6 +9,7 @@ import HelpComponent from "../components/HelpComponent";
 import ContactComponent from "../components/ContactComponent";
 import AboutComponent from "../components/AboutComponent";
 import SearchResultComponent from "../components/SearchResultComponent";
+import PropertyGridComponent from "../components/PropertyGridComponent";
 
 export default class AppContainer extends React.Component {
     render() {
@@ -23,6 +24,7 @@ export default class AppContainer extends React.Component {
                     <Route path="/contact" exact component={ContactComponent}/>
                     <Route path="/about" exact component={AboutComponent}/>
                     <Route path="/search" exact component={SearchResultComponent}/>
+                    <Route path="/properties" exact component={PropertyGridComponent}/>
                 </BrowserRouter>
             </div>
         )
