cbef7c5579a92d0c22f5d07ffc67f938203bb447
Prajakta Dharme <prajakta@Prajaktas-MacBook-Pro.local>
2020-11-29 11:34:42 -0500

    Converting property card component to a class

--- src/components/PropertyCardComponent.js
+++ src/components/PropertyCardComponent.js
@@ -1,34 +1,46 @@
 import React from "react";
 import {Link} from "react-router-dom";
 import "./css/PropertyCardComponent.css"
-import Tooltip from "@material-ui/core/Tooltip";
+import Modal from 'react-modal'
 
-const PropertyCardComponent = ({property}) => {
-    return (
-        // creating a property card
-        <div className="col-3 col-sm-6 col-md-3 wbdv-property-card">
-            <div className="card h-100">
-                <img className="card-img-top" src={property.imageUrl}/>
-                <div className="card-body bg-primary">
-                    <h2>{property.rent}</h2>
-                    <h4 className="card-title">
-                        <Link to={`properties/${property.address}`} title="view the property"
-                              className="wbdv-hyperlink">{property.address}</Link>
-                    </h4>
-                    <p className="card-text text-white">
-                        Available: {property.available} <br/>
-                        Contact: {property.contactInfo}
-                    </p>
-                </div>
-                <div className="card-footer">
-                    <i title="manage appointments"
-                       className="fa fa-address-book wbdv-property-card-icon float-left"></i>
-                    <i title="delete property"
-                       className="fa fa-trash-alt wbdv-property-card-icon float-right"></i>
+const customStyles = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)'
+    }
+};
+
+export default class PropertyCardComponent extends React.Component{
+    render() {
+        return (
+            // creating a property card
+            <div className="col-3 col-sm-6 col-md-3 wbdv-property-card">
+                <div className="card h-100">
+                    <img className="card-img-top" src={this.props.property.imageUrl}/>
+                    <div className="card-body bg-primary">
+                        <h2>{this.props.property.rent}</h2>
+                        <h4 className="card-title">
+                            <Link to={`properties/${this.props.property.address}`} title="view the property"
+                                  className="wbdv-hyperlink">{this.props.property.address}</Link>
+                        </h4>
+                        <p className="card-text text-white">
+                            Available: {this.props.property.available} <br/>
+                            Contact: {this.props.property.contactInfo}
+                        </p>
+                    </div>
+                    <div className="card-footer">
+                        <i title="manage appointments"
+                           className="fa fa-address-book wbdv-property-card-icon float-left"></i>
+                        <i title="delete property"
+                           className="fa fa-trash-alt wbdv-property-card-icon float-right"></i>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
-}
+        )
+    }
 
-export default PropertyCardComponent;
+}
