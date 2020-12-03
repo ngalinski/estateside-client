6babe923f33877fefe4e9442e6e989d1e96ca3b0
Nicholas Galinski <galinski.n@husky.neu.edu>
2020-12-02 22:13:17 -0800

    fixed missing character

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -36,7 +36,7 @@ const AboutComponent = ({state, login, logout, updateSelectedNavItem, toggleProf
                 Built using: React, NodeJS, and the Zillow API.
             </p>
         </div>
-        <footer id="footer">FooterComponent/></footer>
+        <footer id="footer"><FooterComponent/></footer>
     </div>
 
 export default AboutComponent;
