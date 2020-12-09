86633c77323827df3ea10c3cb079b587718cae8a
Prajakta Dharme <prajakta@Prajaktas-MacBook-Pro.local>
2020-12-09 18:24:04 -0500

    Adding the service in individual property component

--- src/components/IndividualPropertyDetailComponent.js
+++ src/components/IndividualPropertyDetailComponent.js
@@ -1,7 +1,27 @@
 import React from "react";
 import "./css/PropertyGridComponent.css"
+import {connect} from "react-redux";
+import PropertyService from "../services/PropertyService";
 
 class IndividualPropertyDetailComponent extends React.Component {
+    state = {
+        property: {},
+    };
+
+    componentDidMount() {
+        const propertyId = this.props.property.zpid
+        console.log(propertyId)
+        this.setState( () => {
+            PropertyService.findPropertyById(propertyId)
+                .then(response => {
+                    console.log(response)
+                    this.setState({
+                                      property: response.bundle,
+                                  })
+                });
+        })
+    }
+
     render() {
         return (
             <div id="booking" className="section">
@@ -11,8 +31,8 @@ class IndividualPropertyDetailComponent extends React.Component {
                             <div className="col-md-7 col-md-push-5">
                                 <div className="booking-cta">
                                     <h1>Details about property</h1>
-                                    <h3>Location: {this.props.propertyTitle}</h3>
-                                    <h4>ZPID: {this.props.propertyZpid}</h4>
+                                    <h3>Location: {this.props.property.address}</h3>
+                                    <h4>ZPID: {this.props.property.zpid}</h4>
                                 </div>
                             </div>
                         </div>
