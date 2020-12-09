df84033116b120db815fd8f039dd6bac3baac434
Zoheb Nawaz <zohebzameer@gmail.com>
2020-12-08 21:37:11 -0500

    Bug fixed

--- src/index.js
+++ src/index.js
@@ -3,21 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import BackgroundSlider from "react-background-slider";
 
 ReactDOM.render(
     <App/>,
     document.getElementById('root')
-);
-
-ReactDOM.render(
-    <BackgroundSlider
-        images={['images/bg01.jpg', 'images/bg02.jpg', 'images/bg03.jpg',
-                 'images/bg04.jpg', 'images/bg05.jpg', 'images/bg01.jpg']}
-        duration={5}
-        transition={1}
-    />,
-    document.getElementById('background-slider')
 );
 
 reportWebVitals();
