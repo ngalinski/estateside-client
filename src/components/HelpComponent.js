7c371581091fc38cd2314e2f0887e15ed06e3efb
Nicholas Galinski <galinski.n@husky.neu.edu>
2020-12-09 00:10:15 -0800

    fixed show/hide faqs

--- src/components/HelpComponent.js
+++ src/components/HelpComponent.js
@@ -21,235 +21,129 @@ const HelpComponent = ({state, login, logout, updateSelectedNavItem, toggleProfi
         <div className="help-body container">
             <h1 className="faqHeader">General Questions</h1>
 
-            <div className="panel-group" id="accordion">
+            <div className="panel-group">
                 <div className="panel panel-default">
-                    <div className="panel-heading">
-                        <h4 className="panel-title">
-                            <a className="accordion-toggle" data-toggle="collapse"
-                               data-parent="#accordion"
-                               href="#collapseOne">Is account registration required?</a>
-                        </h4>
-                    </div>
-                    <div id="collapseOne" className="panel-collapse collapse in">
-                        <div className="panel-body">
-                            Account registration is not required.
-                        </div>
-                    </div>
+                    <input type="checkbox" id="q1"/>
+                    <label htmlFor="q1">Is account registration required?</label>
+                    <span id="content">
+                        Account registration is not required.
+                    </span>
                 </div>
                 <div className="panel panel-default ">
-                    <div className="panel-heading">
-                        <h4 className="panel-title">
-                            <a className="accordion-toggle collapsed" data-toggle="collapse"
-                               data-parent="#accordion"
-                               href="#collapseTen">What is Estateside?</a>
-                        </h4>
-                    </div>
-                    <div id="collapseTen" className="panel-collapse collapse">
-                        <div className="panel-body">
-                            Estateside is a property rental application. Please see the "info" tab
-                            for
-                            more information.
-                        </div>
-                    </div>
+                    <input type="checkbox" id="q2"/>
+                    <label htmlFor="q2">What is Estateside?</label>
+                    <span id="content">
+                        Estateside is a property rental application. Please see the "info" tab for more information.
+                    </span>
                 </div>
                 <div className="panel panel-default">
-                    <div className="panel-heading">
-                        <h4 className="panel-title">
-                            <a className="accordion-toggle collapsed" data-toggle="collapse"
-                               data-parent="#accordion" href="#collapseEleven">Is Estateside
-                                trustworthy?</a>
-                        </h4>
-                    </div>
-                    <div id="collapseEleven" className="panel-collapse collapse">
-                        <div className="panel-body">
-                            Estateside brings renters to properties and properties to renters. We
-                            verify
-                            property managers before listings are posted and verify renters before
-                            they
-                            can make an account.
-                        </div>
-                    </div>
+                    <input type="checkbox" id="q3"/>
+                    <label htmlFor="q3">Is Estateside trustworthy?</label>
+                    <span id="content">
+                        Estateside brings renters to properties and properties to renters. We verify property managers
+                        before listings are posted and verify renters before they can make an account.
+                    </span>
                 </div>
                 <br/>
+
                 <h1 className="faqHeader">Accounts</h1>
+
                 <div className="panel panel-default">
-                    <div className="panel-heading">
-                        <h4 className="panel-title">
-                            <a className="accordion-toggle collapsed" data-toggle="collapse"
-                               data-parent="#accordion" href="#collapseTwo">How do I sign up for an
-                                account?</a>
-                        </h4>
-                    </div>
-                    <div id="collapseTwo" className="panel-collapse collapse">
-                        <div className="panel-body">
-                            Under the "Login" screen, there is an additional place to "Register."
-                        </div>
-                    </div>
+                    <input type="checkbox" id="q4"/>
+                    <label htmlFor="q4">How do I sign up for an account?</label>
+                    <span id="content">
+                        Under the "Login" screen, there is an additional place to "Register."
+                    </span>
                 </div>
                 <div className="panel panel-default">
-                    <div className="panel-heading">
-                        <h4 className="panel-title">
-                            <a className="accordion-toggle collapsed" data-toggle="collapse"
-                               data-parent="#accordion" href="#collapseThree">Do I have to have an
-                                account to schedule an appointment?</a>
-                        </h4>
-                    </div>
-                    <div id="collapseThree" className="panel-collapse collapse">
-                        <div className="panel-body">
-                            An account is required to make an appointment.
-                        </div>
-                    </div>
+                    <input type="checkbox" id="q5"/>
+                    <label htmlFor="q5">Do I have to have an account to schedule an appointment?</label>
+                    <span id="content">
+                        An account is required to make an appointment.
+                    </span>
                 </div>
                 <div className="panel panel-default">
-                    <div className="panel-heading">
-                        <h4 className="panel-title">
-                            <a className="accordion-toggle collapsed" data-toggle="collapse"
-                               data-parent="#accordion" href="#collapseFive">How do I update my
-                                information?</a>
-                        </h4>
-                    </div>
-                    <div id="collapseFive" className="panel-collapse collapse">
-                        <div className="panel-body">
-                            Once logged into your account, select your "profile" and you will be
-                            able to
-                            update your information.
-                        </div>
-                    </div>
+                    <input type="checkbox" id="q6"/>
+                    <label htmlFor="q6">How do I update my information?</label>
+                    <span id="content">
+                        Once logged into your account, select your "profile" and you will be able to update your information.
+                    </span>
                 </div>
                 <div className="panel panel-default">
-                    <div className="panel-heading">
-                        <h4 className="panel-title">
-                            <a className="accordion-toggle collapsed" data-toggle="collapse"
-                               data-parent="#accordion" href="#collapseSix">How do I delete my
-                                account?</a>
-                        </h4>
-                    </div>
-                    <div id="collapseSix" className="panel-collapse collapse">
-                        <div className="panel-body">
-                            From your profile, click the "delete account" button. This will
-                            permanently
-                            delete your account and none of your saved properties or history will be
-                            recoverable.
-                        </div>
-                    </div>
+                    <input type="checkbox" id="q7"/>
+                    <label htmlFor="q7">How do I delete my account?</label>
+                    <span id="content">
+                        From your profile, click the "delete account" button. This will permanently
+                        delete your account and none of your saved properties or history will be recoverable.
+                    </span>
                 </div>
                 <div className="panel panel-default">
-                    <div className="panel-heading">
-                        <h4 className="panel-title">
-                            <a className="accordion-toggle collapsed" data-toggle="collapse"
-                               data-parent="#accordion" href="#collapseEight">How is my information
-                                kept safe?</a>
-                        </h4>
-                    </div>
-                    <div id="collapseEight" className="panel-collapse collapse">
-                        <div className="panel-body">
-                            Estateside will not sell, post, or otherwise use your information
-                            without your
-                            expressed permission. Please refer to our privacy policy for more
-                            information.
-                        </div>
-                    </div>
+                    <input type="checkbox" id="q8"/>
+                    <label htmlFor="q8">How is my information kept safe?</label>
+                    <span id="content">
+                        Estateside will not sell, post, or otherwise use your information without your
+                        expressed permission. Please refer to our privacy policy for more information.
+                    </span>
                 </div>
                 <div className="panel panel-default">
-                    <div className="panel-heading">
-                        <h4 className="panel-title">
-                            <a className="accordion-toggle collapsed" data-toggle="collapse"
-                               data-parent="#accordion" href="#collapseNine">How do I report a
-                                suspicious rental?</a>
-                        </h4>
-                    </div>
-                    <div id="collapseNine" className="panel-collapse collapse">
-                        <div className="panel-body">
-                            Contact Estateside via the "Contact Us" tab.
-                        </div>
-                    </div>
+                    <input type="checkbox" id="q9"/>
+                    <label htmlFor="q9">How do I report a suspicious rental?</label>
+                    <span id="content">
+                        Contact Estateside via the "Contact Us" tab.
+                    </span>
                 </div>
                 <br/>
+
                 <h1 className="faqHeader">Listings</h1>
+
                 <div className="panel panel-default">
-                    <div className="panel-heading">
-                        <h4 className="panel-title">
-                            <a className="accordion-toggle collapsed" data-toggle="collapse"
-                               data-parent="#accordion" href="#collapseFour">How do I search
-                                listings?</a>
-                        </h4>
-                    </div>
-                    <div id="collapseFour" className="panel-collapse collapse">
-                        <div className="panel-body">
-                            Enter a location (either a ZipCode or City,State) in the search bar,
-                            then
+                    <input type="checkbox" id="q10"/>
+                    <label htmlFor="q10">How do I search listings?</label>
+                    <span id="content">
+                            Enter a location (either a ZipCode or City,State) in the search bar, then
                             click the "Search" button.
-                        </div>
-                    </div>
+                    </span>
                 </div>
                 <div className="panel panel-default">
-                    <div className="panel-heading">
-                        <h4 className="panel-title">
-                            <a className="accordion-toggle collapsed" data-toggle="collapse"
-                               data-parent="#accordion" href="#collapseSeven">How can I save
-                                listings I like?</a>
-                        </h4>
-                    </div>
-                    <div id="collapseSeven" className="panel-collapse collapse">
-                        <div className="panel-body">
-                            From the search results page, you will be able to save the listing.
-                        </div>
-                    </div>
+                    <input type="checkbox" id="q11"/>
+                    <label htmlFor="q11">How can I save listings I like?</label>
+                    <span id="content">
+                        From the search results page, you will be able to save the listing.
+                    </span>
                 </div>
                 <div className="panel panel-default">
-                    <div className="panel-heading">
-                        <h4 className="panel-title">
-                            <a className="accordion-toggle collapsed" data-toggle="collapse"
-                               data-parent="#accordion" href="#collapseSeven">What about scams?</a>
-                        </h4>
-                    </div>
-                    <div id="collapseSeven" className="panel-collapse collapse">
-                        <div className="panel-body">
-                            There are a variety of ways to check for scams. Estateside will never
-                            ask you
-                            for money. Additionally, if the property manager is asking for a
-                            MoneyOrder,
-                            Visa Prepaid Card, or other prepaid money option or if the deal seems to
-                            good
-                            to be true, it might be. Be wary of scams and use your best judgement.
-                        </div>
-                    </div>
+                    <input type="checkbox" id="q12"/>
+                    <label htmlFor="q12">What about scams?</label>
+                    <span id="content">
+                        There are a variety of ways to check for scams. Estateside will never ask you
+                        for money. Additionally, if the property manager is asking for a MoneyOrder,
+                        Visa Prepaid Card, or other prepaid money option or if the deal seems to good
+                        to be true, it might be. Be wary of scams and use your best judgement.
+                    </span>
                 </div>
                 <br/>
+
                 <h1 className="faqHeader">Contacting a Property</h1>
+
                 <div className="panel panel-default">
-                    <div className="panel-heading">
-                        <h4 className="panel-title">
-                            <a className="accordion-toggle collapsed" data-toggle="collapse"
-                               data-parent="#accordion" href="#collapseFour">How do I contact a
-                                property?</a>
-                        </h4>
-                    </div>
-                    <div id="collapseFour" className="panel-collapse collapse">
-                        <div className="panel-body">
-                            From a property page, you can make an appointment to view a property.
-                        </div>
-                    </div>
+                    <input type="checkbox" id="q13"/>
+                    <label htmlFor="q13">How do I contact a property?</label>
+                    <span id="content">
+                        From a property page, you can make an appointment to view a property.
+                    </span>
                 </div>
                 <div className="panel panel-default">
-                    <div className="panel-heading">
-                        <h4 className="panel-title">
-                            <a className="accordion-toggle collapsed" data-toggle="collapse"
-                               data-parent="#accordion" href="#collapseSeven">I've seen a property.
-                                What's next?</a>
-                        </h4>
-                    </div>
-                    <div id="collapseSeven" className="panel-collapse collapse">
-                        <div className="panel-body">
-                            At this point, if you like property, you can work with the property
-                            manager
-                            for further steps. If you didn't like it, there's no obligation!
-                        </div>
-                    </div>
+                    <input type="checkbox" id="q14"/>
+                    <label htmlFor="q14">I've seen a property. What's next?</label>
+                    <span id="content">
+                        At this point, if you like property, you can work with the property manager
+                        for further steps. If you didn't like it, there's no obligation!
+                    </span>
                 </div>
+
             </div>
+            <FooterComponent/>
         </div>
-        <FooterComponent/>
     </div>
 
 export default HelpComponent
