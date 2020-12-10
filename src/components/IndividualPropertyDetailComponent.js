bcfeaa6a780cf07a2bbbf8c95484312a296ec463
pdharme <dharme.p@husky.neu.edu>
2020-12-10 12:28:53 -0500

    Enhancing the visuals

--- src/components/IndividualPropertyDetailComponent.js
+++ src/components/IndividualPropertyDetailComponent.js
@@ -27,39 +27,74 @@ class IndividualPropertyDetailComponent extends React.Component {
             <div id="booking" className="section">
                 <div className="section-center">
                     <div className="container">
-                        <div className="row">
-                            <div className="col-md-4 col-md-pull-7">
-                                <div className="property-detail">
-                                    <h1>Details</h1>
-                                    <h4> Address: {this.state.property.address}</h4>
+                        <div>
+                            <div className="property-detail">
+                                <h1>Details</h1>
+                                <table>
+                                    <tr>
+                                        <td>
+                                            <h6>Address</h6>
+                                        </td>
+                                        <td>
+                                            &nbsp;
+                                        </td>
+                                        <td>
+                                            {this.state.property.address}
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <h6>Owners</h6>
+                                        </td>
+                                        <td>
+                                            &nbsp;
+                                        </td>
+                                        <td>
+                                            {this.state.property.ownerName}
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <h6>Coordinates</h6>
+                                        </td>
+                                        <td>
+                                            &nbsp;
+                                        </td>
+                                        <td>
+                                            {this.state.property.coordinates &&
+                                             this.state.property.coordinates[1]},
+                                            {this.state.property.coordinates &&
+                                             this.state.property.coordinates[0]}
 
-                                    <h6> Latitude: {this.state.property.coordinates &&
-                                                    this.state.property.coordinates[1]}</h6>
-                                    <h6> Longitude: {this.state.property.coordinates &&
-                                                     this.state.property.coordinates[0]}</h6>
-                                    <h6>Owner name: {this.state.property.ownerName}</h6>
-                                    <h6>ZPID: {this.state.property.zpid}</h6>
-                                    <p>Our property appointments can be booked here. Once you make
-                                        an
-                                        appointment, a showing agent will contact you to confirm
-                                        the site visit. Feel free to ask us any questions about the
-                                        Property during the visit appointment.
-                                    </p>
-                                </div>
+                                        </td>
+                                    </tr>
+
+                                    <tr>
+                                        <td>
+                                            <h6>ZPID</h6>
+                                        </td>
+                                        <td>
+                                            &nbsp; &nbsp; &nbsp;
+                                        </td>
+                                        <td>
+                                            {this.state.property.zpid}
+                                        </td>
+                                    </tr>
+                                </table>
                             </div>
-                            <div className="col-md-7 col-md-push-5">
-                                <div className="booking-form">
-                                    {
-                                        this.state.property.coordinates &&
-                                        <MapComponent latitude={parseFloat(this.state.property.coordinates[1])}
-                                                      longitude={parseFloat(this.state.property.coordinates[0])
-                                        }/>
-                                    }
-                                    {/*<MapComponent latitude={this.state.property.coordinates &&*/}
-                                    {/*                            parseFloat(this.state.property.coordinates[0])}*/}
-                                    {/*                  longitude={this.state.property.coordinates &&*/}
-                                    {/*                             parseFloat(this.state.property.coordinates[1])}/>*/}
-                                </div>
+                        </div>
+                        <div >
+                            <div className="booking-form">
+                                {
+                                    this.state.property.coordinates &&
+                                    <MapComponent latitude={parseFloat(this.state.property.coordinates[1])}
+                                                  longitude={parseFloat(this.state.property.coordinates[0])
+                                                  }/>
+                                }
+                                {/*<MapComponent latitude={this.state.property.coordinates &&*/}
+                                {/*                            parseFloat(this.state.property.coordinates[0])}*/}
+                                {/*                  longitude={this.state.property.coordinates &&*/}
+                                {/*                             parseFloat(this.state.property.coordinates[1])}/>*/}
                             </div>
                         </div>
                     </div>
