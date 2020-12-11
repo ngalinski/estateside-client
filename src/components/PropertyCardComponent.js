f3696c4b8a09cf6b92a552b9a28ebb83e0e5a317
Japher Su <japhersu@Japhers-MacBook-Pro.local>
2020-12-10 20:50:23 -0500

    added random property images

--- src/components/PropertyCardComponent.js
+++ src/components/PropertyCardComponent.js
@@ -42,7 +42,9 @@ export default class PropertyCardComponent extends React.Component {
     }
 
     render() {
-        const imageUrl = "https://picsum.photos/300/200";
+        let randomNum = Math.random() * 100;
+        const imageUrl = `https://source.unsplash.com/collection/1896718/300x200/?sig=${randomNum}`
+        // const imageUrl = "https://picsum.photos/300/200"; // use this if the other image url fails to load images
         return (
             // creating a property card
             <div className="col-sm-6 col-md-4 col-lg-3 wbdv-property-card">
